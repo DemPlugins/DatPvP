2c1e604e29817c6d0b4218523586afc96bcd375c
adamjones57 <ar98jones@gmail.com>
2012-10-07 23:45:49 -0500

    Added licensing.

--- src/in/tech57gam/datpvp/DatPvP.java
+++ src/in/tech57gam/datpvp/DatPvP.java
@@ -1,8 +1,14 @@
+
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
+
 package in.tech57gam.datpvp;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -13,12 +19,12 @@ public class DatPvP extends JavaPlugin
 	
 	public void onEnable()
 	{
-		getLogger().info("PvPlugin has been enabled.");
+		getLogger().info("DatPvP has been enabled.");
 	}
 	
 	public void onDisable()
 	{
-		getLogger().info("PvPlugin has been disabled!");
+		getLogger().info("DatPvP has been disabled!");
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
@@ -39,12 +45,8 @@ public class DatPvP extends JavaPlugin
 			Bukkit.getServer().dispatchCommand((Bukkit.getConsoleSender()), "manuadd " + player.getName() + " Private test_pvp_world");
 			return true;
 			}
-		} // End Enlist Block.
-		
-		
+	
+		}
 		return false;
 	}
-
 }
-/*
- * COCK
